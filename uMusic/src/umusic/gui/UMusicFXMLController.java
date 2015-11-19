1d8d46ee1e5c69b95a0317e310bf3bcb8cdeb632
Donna <Donna@Dolce.home>
2015-11-18 22:23:01 -0500

    add more parts of the GUI

--- uMusic/src/umusic/gui/UMusicFXMLController.java
+++ uMusic/src/umusic/gui/UMusicFXMLController.java
@@ -5,12 +5,17 @@
  */
 package umusic.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 
 /**
  *
@@ -21,12 +26,20 @@ public class UMusicFXMLController implements Initializable {
     @FXML
     private Label label;
     
+    @FXML BorderPane root;
+    
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
     
+    @FXML
+    private void addTrack(ActionEvent event) throws IOException {
+    Object track = FXMLLoader.load(getClass().getResource("gui/UMusicFXML.fxml"));
+
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
