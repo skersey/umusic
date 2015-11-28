d231d551630fc9189a5f15c34cb5162f50fe6268
bruce.sailer <bruce.sailer@HQSTLT1467077.nmfs.local>
2015-11-27 23:58:05 -0500

    modify editNote in uMusicTrack so that it will replace a note at a given index and added deleteNote to remove a note at a given index.

--- uMusic/src/umusic/uMusicTrack.java
+++ uMusic/src/umusic/uMusicTrack.java
@@ -10,81 +10,88 @@ import org.jfugue.midi.MidiDictionary;
  * @author bkersey
  */
 public class uMusicTrack {
-	private int volume = 100;
-	private String instrument = "Piano";	
-	private String trackName;
-	private String trackString;
-	private TrackNumber trackNumber;
-	private final ArrayList <uMusicNote> notes = new ArrayList<>();
-        public ArrayList<String> InstrumentList = new ArrayList<>(Arrays.asList(
-		 "Flute", "Piano", "Guitar", "Alto_Sax", "Soprano_Sax", "Clarinet",
-		 "Violin", "Tuba", "Oboe", "Acoustic_Bass"));
-		
-        uMusicTrack(TrackNumber trackNumber) {
-            this(trackNumber, null);
+
+    private int volume = 100;
+    private String instrument = "Piano";
+    private String trackName;
+    private String trackString;
+    private TrackNumber trackNumber;
+    private final ArrayList<uMusicNote> notes = new ArrayList<>();
+    public ArrayList<String> InstrumentList = new ArrayList<>(Arrays.asList(
+            "Flute", "Piano", "Guitar", "Alto_Sax", "Soprano_Sax", "Clarinet",
+            "Violin", "Tuba", "Oboe", "Acoustic_Bass"));
+
+    uMusicTrack(TrackNumber trackNumber) {
+        this(trackNumber, null);
+    }
+
+    public enum TrackNumber {
+        TRACK0, TRACK1, TRACK2, TRACK3, TRACK4, TRACK5, TRACK6, TRACK7, TRACKMAX
+    };
+
+    public uMusicTrack(TrackNumber trackNumber, String name) {
+        this.trackNumber = trackNumber;
+        this.trackName = name;
+    }
+
+    public int setVolume(int volume) {
+        if (volume > 125 || volume < 0) {
+            return -1;
         }
 
-	public enum TrackNumber {TRACK0, TRACK1, TRACK2, TRACK3, TRACK4, TRACK5, TRACK6, TRACK7, TRACKMAX};
-	
-	public uMusicTrack (TrackNumber trackNumber, String name) {
-		this.trackNumber = trackNumber;
-		this.trackName = name;
-	}
+        this.volume = volume;
+        return 0;
+    }
 
-	public int setVolume (int volume) {
-		if (volume > 125 || volume < 0)
-			return -1;
+    public int setInstrument(String i) {
+        if (InstrumentList.contains(i) == false) {
+            System.out.println("The instrument is not valid: " + i);
+            return -1;
+        }
 
-		this.volume = volume;
-		return 0;
-	}
-	
-	public int setInstrument (String i) {
-		if (InstrumentList.contains(i) == false) {
-		    System.out.println("The instrument is not valid: " + i );	
-		    return -1;
-	        }
+        this.instrument = i;
+        return 0;
+    }
 
-		this.instrument = i;
-	    	return 0;
-	}
+    public int addNextNote(uMusicNote n) {
+        notes.add(n);
+        return 0;
+    }
 
-	public int addNextNote (uMusicNote n) {
-		notes.add(n);
-		return 0;
-	}
+    public void editNote(int arrayIndex, uMusicNote note) {
+        notes.set(arrayIndex, note);
+    }
 
-	public void editNote (int arrayIndex, uMusicNote note) {
-		notes.add(arrayIndex, note);
-	}
-	
-	public void deleteLastNote () {
-		notes.remove(notes.size()-1);
-	}
+    public void deleteNote(int arrayIndex) {
+        notes.remove(arrayIndex);
+    }
+    public void deleteLastNote() {
+        notes.remove(notes.size() - 1);
+    }
 
-	public String getTrackName() {
-		return trackName;	
-	}
-	
-	public ArrayList<uMusicNote> getTrackNotes() {
-		return (ArrayList<uMusicNote>)notes.clone();
-	}
+    public String getTrackName() {
+        return trackName;
+    }
 
-	public ArrayList<String> getSupportedInstruments() {
-		return (ArrayList<String>) InstrumentList.clone();
-	}
-	
-	public String buildTrackString () {
-		trackString  = " V" + trackNumber.ordinal();
-		trackString += " :CON(7," + volume + ")";
-		trackString += " I" + MidiDictionary.INSTRUMENT_STRING_TO_BYTE.get(instrument.toUpperCase());
-		
-		Iterator <uMusicNote> iter = notes.iterator();
-		while (iter.hasNext()) {
-			uMusicNote next = iter.next();
-			trackString+=next.toString();
-		}
+    public ArrayList<uMusicNote> getTrackNotes() {
+        return (ArrayList<uMusicNote>) notes.clone();
+    }
 
-		return trackString;
-	}
+    public ArrayList<String> getSupportedInstruments() {
+        return (ArrayList<String>) InstrumentList.clone();
+    }
+
+    public String buildTrackString() {
+        trackString = " V" + trackNumber.ordinal();
+        trackString += " :CON(7," + volume + ")";
+        trackString += " I" + MidiDictionary.INSTRUMENT_STRING_TO_BYTE.get(instrument.toUpperCase());
+
+        Iterator<uMusicNote> iter = notes.iterator();
+        while (iter.hasNext()) {
+            uMusicNote next = iter.next();
+            trackString += next.toString();
+        }
+
+        return trackString;
+    }
 }
