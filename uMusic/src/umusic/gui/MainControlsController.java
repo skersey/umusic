93b62f4eef173d6d253b07ef039bfce083a94460
dv8snatch22 <mcnallydj@hotmail.com>
2015-11-29 12:51:39 -0500

    Add saveSong() and openSong()

--- uMusic/src/umusic/gui/MainControlsController.java
+++ uMusic/src/umusic/gui/MainControlsController.java
@@ -5,33 +5,25 @@
  */
 package umusic.gui;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.stage.Popup;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
-import javafx.stage.Window;
-import javafx.stage.WindowEvent;
 import umusic.uMusicAppData;
+import umusic.uMusicFileController;
 
 /**
  *
@@ -65,7 +57,19 @@ public class MainControlsController implements Initializable {
             Logger.getLogger(MainControlsController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    @FXML
+    private void saveSong() throws IOException {
+        uMusicFileController fc = new umusic.uMusicFileController(umusic.UMusic.sc);
+        fc.save_gui();       
+    }
   
+    @FXML
+    private void openSong() throws IOException {
+        uMusicFileController fc = new umusic.uMusicFileController(umusic.UMusic.sc);
+        umusic.UMusic.sc = fc.open_gui();
+        
+    }
     
     @FXML
     private void addTrack(ActionEvent event) throws IOException {
