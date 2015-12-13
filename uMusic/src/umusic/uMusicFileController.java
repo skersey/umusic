65a5f122619715afc4728ed5a66277a66804fe34
Brian Kersey <s_kersey@comcast.net>
2015-12-12 19:24:16 -0500

    -removed some unused includes

--- uMusic/src/umusic/uMusicFileController.java
+++ uMusic/src/umusic/uMusicFileController.java
@@ -3,29 +3,18 @@ package umusic;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
-import java.nio.file.Files;
-import javafx.stage.FileChooser;
 
 /**
  *
  * @author Joshua
  */
 public class uMusicFileController {
-    
-//    private uMusicSongController sc;
-//    String json;
-//    
-//    public uMusicFileController(uMusicSongController sc){
-//        this.sc = sc;
-//    }
     
     public static void save(String fileName, uMusicSongController sc){
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -53,6 +42,4 @@ public class uMusicFileController {
         }
         return sc;
     }
-    
-   
 }
